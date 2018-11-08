826142483c835753e7fc2b8c14e242ee6108c013
sakuyuki89 <stargreens@hotmail.com>
2018-11-08 02:52:22 +0000

    Issue #18 throwに関する不具合を対応

--- opd-test-js/server.js
+++ opd-test-js/server.js
@@ -176,7 +176,7 @@ async function waitAllForHiraganaInsertWrapper(req, res, array, vInsertTarget) {
 	res.status(200).json(array);
 }
 
-/*　オリジナルバージョン
+/*　佐野さんオリジナルバージョン
 app.post('/Inbound', function (req, res) {
 	console.log("Hello!");
 	var InRailway = req.body.conversation.memory.line.value;
@@ -269,18 +269,11 @@ app.post('/Inbound', function (req, res) {
 			InTime : req.body.conversation.memory.time.value,
 			InIsHoliday : req.body.conversation.memory.isHoliday.value
 		};
-	/*
-		var InSearchTerm = {
-			InRailway : "半蔵門線",
-			InStationOn : "渋谷",
-			InStationOff : "九段下",
-			InTime : "8時15分",
-			InIsHoliday : "平日"
-		};
-	*/	
-		if(trace_level.Common == 1){
-			console.log("[logs] Railway:" + InSearchTerm.InRailway + ", StationOn: " + InSearchTerm.InStationOn + ", StationOff: " + InSearchTerm.InStationOff + ", Time" + InSearchTerm.InTime + ", IsHoliday: " + InSearchTerm.InIsHoliday);
+	
+		if(trace_level.Common >= 1){
+			console.log("[logs] Railway:" + InSearchTerm.InRailway + ", StationOn: " + InSearchTerm.InStationOn + ", StationOff: " + InSearchTerm.InStationOff + ", Time: " + InSearchTerm.InTime + ", IsHoliday: " + InSearchTerm.InIsHoliday);
 		}
+		
 		/*平日・休日フラグをodptフォーマットに変換*/
 		if(InSearchTerm.InIsHoliday == "休日"){
 			InSearchTerm.InOdptCalendar = 'odpt.Calendar:SaturdayHoliday';
@@ -290,14 +283,9 @@ app.post('/Inbound', function (req, res) {
 	
 		// InTimeの整形ロジック
 		// Issue #15
-		// 8時15分で来る場合
+		// 8時15分で来る場合 momentはHH:mm, HH, HHmm, HH時mm分のいづれも対応できていた。
 		var InTimeFormatted = moment(InSearchTerm.InTime,'HH時mm分');
 		var MaxTimeFormatted = moment(InSearchTerm.InTime,'HH時mm分').add(1,'hours');
-		/*
-		// 半角数字で来る場合 InTime = 8
-		InTimeFormatted = moment('00:00');
-		InTimeFormatted.add(InTime,'hours');
-		*/
 		
 		//方面情報取得
 		/*
@@ -312,18 +300,15 @@ app.post('/Inbound', function (req, res) {
 			if (err) {
 				var ExceptionDatabasePrepError = {
 					category : "dbError", 
-					message: "SQLのPREPAREでエラーがおきたよん: " + err.toString(),	
+					message: "SQLのPREPAREでエラーがおきたよん: " + err.toString()
 				};
 				throw ExceptionDatabasePrepError;
-				//res.type("text/plain").status(500).send("ERROR: " + err.toString());
 			}
-			//statement.exec([InRailway], function (err, STResult) {
 			statement.exec([], function (err, STResult) {
 				if (err) {
-					//res.type("text/plain").status(500).send("ERROR: " + err.toString());
 					var ExceptionDatabaseExecError = {
 						category : "dbError", 
-						message: "SQLの実行でエラーがおきたよん" + err.toString(),	
+						message: "SQLの実行でエラーがおきたよん" + err.toString()
 					};
 					
 					throw ExceptionDatabaseExecError;
@@ -332,8 +317,7 @@ app.post('/Inbound', function (req, res) {
 					//路線情報が取得できない(路線・駅名などが間違っている)
 					var ErrorUnableGetRailway = {
 						category : "noResult", 
-						message: "何か入力エラーだのねん。",	
-
+						message: "何か入力エラーだのねん。"	
 					};
 					var httpResponse = {
 						"replies": [
@@ -343,10 +327,14 @@ app.post('/Inbound', function (req, res) {
 							}
 						]
 					};	
+					if(trace_level.Common >= 1){
+						console.logs("何か入力エラーだのねん。");
+					}
 					res.status(200).json(httpResponse);
 					return;
-						//throw ErrorUnableGetRailway;
+					//throw ErrorUnableGetRailway;　エラーになるのでとりあえず、正常終了としてエラーを返している
 				}
+
 				/*時刻表取得*/
 				var paramApiStation = {
 					aclConsumerKey		: "d70b73e46ff972f7f4c7aa5f0729cec27739b8a74fcae80925ea074bd60ebb0e",
@@ -372,14 +360,15 @@ app.post('/Inbound', function (req, res) {
 					getRes.on('data', function (chunk) {
 						body += chunk;
 					});
+					
 					getRes.on('end', function () {
 						var oBody = JSON.parse(body);
 						if(!oBody[0]){
-							console.log("Error: Cannot get station Timetable with below URL;");
+							console.log("Error: Cannot get Station Timetable with below URL;");
 							console.log(URL_stationTimetable);
 							var ErrorUnableGetAPI = {
 								category : "apiError", 
-								message: "APIからデータがとれなかったのねん",	
+								message: "API StationTimetable からデータがとれなかったのねん"
 							};
 							var httpResponse = {
 								"replies": [
@@ -391,6 +380,7 @@ app.post('/Inbound', function (req, res) {
 							};	
 							res.status(200).json(httpResponse);
 							return;
+							//throw ErrorUnableGetAPI;　エラーになるのでとりあえず、正常終了としてエラーを返している
 						}
 						var odptResult = oBody[0]["odpt:stationTimetableObject"];
 						
@@ -410,9 +400,10 @@ app.post('/Inbound', function (req, res) {
 							if(departureTime.isBetween(InTimeFormatted,MaxTimeFormatted))
 								TimeTables.push(TimeTable);
 						});
-						if(trace_level.Common == 1){
+						if(trace_level.Common >= 1){
 							console.log("取得Train数: "+TimeTables.length);
 						}
+						
 						/*Timetable 行先で絞込み*/
 						var promise_timetables_dest = [];
 						TimeTables.forEach(function (timetable){
@@ -435,11 +426,11 @@ app.post('/Inbound', function (req, res) {
 										//console.log(body);
 										var oBody = JSON.parse(body);
 										if(!oBody[0]){
-											console.log("Error: Cannot get station Timetable with below URL;");
+											console.log("Error: Cannot get Train Timetable with below URL;");
 											console.log(URL_stationTimetable);
 											var ErrorUnableGetAPI = {
 												category : "apiError", 
-												message: "APIからデータがとれなかったのねん",	
+												message: "API TrainTimetable からデータがとれなかったのねん"
 											};
 											var httpResponse = {
 												"replies": [
@@ -448,16 +439,17 @@ app.post('/Inbound', function (req, res) {
 														"content": ErrorUnableGetAPI.message
 													}
 												]
-											};	
+											};
 											res.status(200).json(httpResponse);
 											return;
+											//throw ErrorUnableGetAPI;　エラーになるのでとりあえず、正常終了としてエラーを返している
 										}										
 										
 										var odptResult = oBody[0]["odpt:trainTimetableObject"];
 										var is_stopped = false
 										
 										var promise_train = [];
-										if(trace_level.Destination ==2){
+										if(trace_level.Destination >=2){
 													console.log("[DEBUG]"+timetable);
 													console.log("[DEBUG]"+URL_trainTimetable);
 										}
@@ -471,7 +463,7 @@ app.post('/Inbound', function (req, res) {
 											}
 											if(element["odpt:arrivalStation"] == paramApiStation.odptStationOff){
 												is_stopped = true;
-												if(trace_level.Destination == 2){
+												if(trace_level.Destination >= 2){
 													console.log("[DEBUG] Stopped");
 												}											
 											}
@@ -482,10 +474,10 @@ app.post('/Inbound', function (req, res) {
 										if(is_stopped == true){
 											resolve(timetable);
 										}else{
-											if(trace_level.Destination == 2){
+											if(trace_level.Destination >= 2){
 												console.log("[DEBUG] NOT Stopped");
 											}
-											resolve("NS");
+											resolve("NS"); //降車駅に停まらない電車
 										}
 									});
 								}).on('error', function (err) {
@@ -497,7 +489,7 @@ app.post('/Inbound', function (req, res) {
 						Promise.all(promise_timetables_dest).then(function(TimeTables_dest){
 							/*タイプ(Express->急行, Local->普通)変換*/
 							var promise_timetables_type = [];
-							if(trace_level.Common == 1){
+							if(trace_level.Common >= 1){
 								console.log("取得Train数(Dest): "+TimeTables_dest.length);
 							}
 							TimeTables_dest.forEach(function(timetable){
@@ -520,18 +512,18 @@ app.post('/Inbound', function (req, res) {
 												
 												
 												//終着駅の行先日本語名を取得
+												//メモ：Stationテーブルをひらがな対応で一意にならない場合に
+												//Stationtテーブルで正かあいまい対応なのか判別させる不具合が必要(テーブルにAPIから取得したレコードについてはフラグを立てる？)
+												
 												//SELECT TOP 1 "InboundWord" as "Destination" FROM "opd-test.opd-test-db::tables.Station" WHERE "odptStation" ='odpt.Station:TokyoMetro.Hanzomon.Kudanshita'
 												var sql_getDestination = 'SELECT TOP 1 "InboundWord" as "Destination" FROM "opd-test.opd-test-db::tables.Station" '
 													+ 'WHERE "odptStation"=\'' + timetable["odptDestinationStation"] + '\''; 
 												req.db.prepare(sql_getDestination, function (err, statement) {
 													if (err) {
 														throw "PREPAREで何かDBエラーだよ" + err.toString();
-														//res.type("text/plain").status(500).send("ERROR: " + err.toString());
 													}
-													//statement.exec([InRailway], function (err, STResult) {
 													statement.exec([], function (err, STResult) {
 														if (err) {
-															//res.type("text/plain").status(500).send("ERROR: " + err.toString());
 															throw "PREPAREで何かDBエラーだよ" + err.toString();
 														}
 														if (STResult.length ){
@@ -572,7 +564,7 @@ app.post('/Inbound', function (req, res) {
 										}
 									]
 								};
-								console.log("Here");
+								console.log("HTTP Response:");
 								console.log(httpResponse);
 								res.status(200).json(httpResponse);
 							})
@@ -602,6 +594,8 @@ app.post('/Inbound', function (req, res) {
 		}
 	}
 });
+
+
 
 app.get('/insertRailway', function (req, res) {
 	var URL =
